0bff7aa74e0c7b0f24f74d892114e0e4d8e975b4
aldusd <aldusdanielfsm@gmail.com>
2023-07-11 14:30:40 -0300

    fix: allows maven to run tests

--- src/main/java/com/sh/fastmanagementdemoapi/HealthController.java
+++ src/main/java/com/sh/fastmanagementdemoapi/HealthController.java
@@ -1,4 +1,4 @@
-package com.sh.fastmanagementdemoapi.controllers;
+package com.sh.fastmanagementdemoapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
