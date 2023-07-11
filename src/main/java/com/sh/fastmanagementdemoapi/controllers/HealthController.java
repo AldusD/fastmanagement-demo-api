6914192dd5ca3434a0dd42be016f678a3776b984
aldusd <aldusdanielfsm@gmail.com>
2023-07-11 12:11:58 -0300

    test: tests workflow

---
+++ src/main/java/com/sh/fastmanagementdemoapi/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.sh.fastmanagementdemoapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkApi() throws InterruptedException {
+        return "OK";
+    }
+}
---
+++ src/test/java/com/sh/fastmanagementdemoapi/controllers/HealthControllerTest.java
@@ -0,0 +1,23 @@
+package com.sh.fastmanagementdemoapi.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+public class HealthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testHealth () throws Exception {
+        mockMvc.perform(get("/health").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
