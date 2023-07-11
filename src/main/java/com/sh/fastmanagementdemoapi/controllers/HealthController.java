50d7263a37908a0f5643c69790c0e125198642c8
aldusd <aldusdanielfsm@gmail.com>
2023-07-11 15:02:36 -0300

    test: tests deploy

--- src/main/java/com/sh/fastmanagementdemoapi/controllers/HealthController.java
+++ src/main/java/com/sh/fastmanagementdemoapi/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package com.sh.fastmanagementdemoapi;
+package com.sh.fastmanagementdemoapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
